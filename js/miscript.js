b8f2d49b1e50fbc6362c0344db35ccb53db28c38
Alexander032022 <75050925@certus.edu.pe>
2022-10-25 22:10:45 -0500

    version 8.0

---
+++ js/miscript.js
@@ -0,0 +1,10 @@
+//alert("Bienvenido a JS");
+//document.write("Hola JS");
+//console.log("Esto en la consola")
+
+/* let x=parseInt(prompt("Ingrese un primer numero")); //declarar la variable x
+let y=parseInt(prompt("ingrese un segundo numeros"));
+alert("la suma es: " + (x+y)); */
+
+let nombre=prompt("Ingrese tu nombre");
+alert("Bienvenido a mi pagina" + nombre + "espero que le guste");
